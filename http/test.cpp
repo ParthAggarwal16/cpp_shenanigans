d6ff7cc05c948d4225d4cd2c2e68f823482f8383
ParthAggarwal16 <parth22012006@gmail.com>
2026-09-22 13:00:39 +0530

    copy semantics

--- http/test.cpp
+++ http/test.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 #include <vector>
+using namespace std;
 
 int main() {
-  std::vector<bool> vecbool = {true, false, true};
-  bool *p = &vecbool[0];
-  std ::cout << "before" << p << "\n";
-
-  for (int i = 0; i < 20; i++) {
-    v.push_back(i);
-  }
-  std::cout << "after: " << *p << "\n";
+  std::string a = "hello there";
+  std::string b = a;
+  cout << a << endl;
+  cout << b;
 }
