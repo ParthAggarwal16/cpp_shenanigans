36a1717a73419993f7f4180fc982531533df36ea
ParthAggarwal16 <parth22012006@gmail.com>
2026-09-24 22:37:53 +0530

    making a constructor

--- http/move.cpp
+++ http/move.cpp
@@ -3,7 +3,11 @@
 
 class Buffer {
 private:
-  char *data;
+  char *data; // basicallt represents a pointer which can store an adrress of a
+              // char object
+
+public:
+  Buffer(const char *text) { data = new char(strlen(text) + 1); };
 };
 
 int main() { return 0; }
