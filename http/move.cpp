a8dd651941e52fee9e7aa3787d0a6ebd1f690b54
ParthAggarwal16 <parth22012006@gmail.com>
2026-09-24 22:44:59 +0530

    undoing the exp and doing it again

--- http/move.cpp
+++ http/move.cpp
@@ -5,20 +5,6 @@ class Buffer {
 private:
   char *data; // basicallt represents a pointer which can store an adrress of a
               // char object
-
-public:
-  Buffer(const char *text) { data = new char(strlen(text) + 1); };
-
-  ~Buffer() {
-    delete[] data;
-    std::cout << "Destructor\n";
-  }
-
-  void print() { std::cout << data << "\n"; }
 };
 
-int main() {
-  Buffer a("Hello");
-  Buffer b = a;
-  b.print();
-}
+int main() { return 0; }
